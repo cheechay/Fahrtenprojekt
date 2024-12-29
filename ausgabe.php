cb04857679f4e147ae3591b02f83be6dbf9142b7
miramaster <zhazmirab@gmail.com>
2024-12-29 21:21:27 +0100

    verbesserung in ausgabe.php + delete Button arbeit mit xml

--- ausgabe.php
+++ ausgabe.php
@@ -20,6 +20,7 @@
                <th>Kilometer Ende</th>
                <th>Kilometer Differenz</th>
                <th>Details</th>
+               <th>Aktion</th>
            </thead>
 
             <tbody>
@@ -34,7 +35,49 @@
                         $id = isset($fahrt['id']) ? $fahrt['id'] : ''; 
                         $delete_id = isset($fahrt['id']) ? $fahrt['id'] : ''; 
                         
+
+                        $fahrtenArray = [];
+
                         foreach ($xml->fahrten as $fahrt) {
+                            $fahrtenArray[] = $fahrt;
+                        }
+                        if (isset($_GET['delete_id'])) {
+                            $delete_id = $_GET['delete_id'];
+    
+                            // Durchlaufe das Array und finde den entsprechenden Eintrag
+                            foreach ($fahrtenArray as $key => $fahrt) {
+                                if ($fahrt['id'] == $delete_id) {
+                                    unset($fahrtenArray[$key]); // Lösche den Eintrag
+                                    break;
+                                }
+                            }
+    
+                            // Schreibe die geänderte XML-Datei zurück
+                            $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><fahrtenbuch></fahrtenbuch>'); // Leere XML-Struktur
+                            foreach ($fahrtenArray as $fahrt) {
+                                $fahrten = $xml->addChild('fahrten');
+                                $fahrten->addAttribute('id', (string)$fahrt['id']);
+                                $fahrten->addChild('name', (string)$fahrt->name);
+                                $fahrten->addChild('wohnort', (string)$fahrt->wohnort);
+                                $fahrten->addChild('zweck', (string)$fahrt->zweck);
+                                $fahrten->addChild('datum', (string)$fahrt->datum);
+                                $fahrten->addChild('uhrzeit_vom', (string)$fahrt->uhrzeit_von);
+                                $fahrten->addChild('uhrzeit_bis', (string)$fahrt->uhrzeit_bis);
+                                $fahrten->addChild('km_start', (string)$fahrt->km_start);
+                                $fahrten->addChild('km_end', (string)$fahrt->km_end);
+                                $fahrten->addChild('kmdiff', (string)$fahrt->kmdiff);
+                            }
+    
+                            // Speichere die geänderte XML-Datei
+                            $xml->asXML($xmlFile);
+    
+                            // Seite neu laden, um den aktualisierten Stand anzuzeigen
+                            header("Location: " . $_SERVER['PHP_SELF']);
+                            exit;
+                        }
+
+
+                        foreach ($fahrtenArray as $fahrt) {
                             
                             $id = (string)$fahrt['id'];
                             $name = $fahrt->name;
@@ -59,7 +102,9 @@
                                         <td>$km_end</td>
                                         <td>$kmdiff</td>
                                         <td><a href=\"detail.php?id=$id\">Detail</a></td>
-                                     
+                                        <td>
+                                            <a href='?delete_id=$id' class='btn-delete' onclick='return confirm(\"Möchten Sie diesen Eintrag wirklich löschen?\")'>Delete</a>
+                                        </td>
                                       </tr>
                                 ";
                                 echo $tableRow;
