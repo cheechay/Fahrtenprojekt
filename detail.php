baeee83e1029692c2b7e8cc92f0fc795e68a3359
Priyanka <112107778+pkc6560@users.noreply.github.com>
2025-01-04 20:16:01 +0100

    Interface Anpassung  + Icon für Button zugefügt

--- detail.php
+++ detail.php
@@ -6,79 +6,121 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ID Liste</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
 </head>
 
 <body>
 
-<?php
+    <?php
 
-// Überprüfen, ob die ID über die URL übergeben wurde
-if (isset($_GET['id'])) {
-    $id = $_GET['id']; // ID aus der URL holen
+    // Überprüfen, ob die ID über die URL übergeben wurde
+    if (isset($_GET['id'])) {
+        $id = $_GET['id']; // ID aus der URL holen
+    
+        // XML-Datei laden
+        $xmlFile = 'fahrtenbuch.xml';
+        $xml = simplexml_load_file($xmlFile) or die("Error: Cannot create object");
 
-    // XML-Datei laden
-    $xmlFile = 'fahrtenbuch.xml';
-    $xml = simplexml_load_file($xmlFile) or die("Error: Cannot create object");
+        //prüfen ob die ID die höchste ist
+        $ids = [];
+        foreach ($xml->fahrten as $fahrt) {
+            $ids[] = (string) $fahrt['id'];
+        }
+        $maxId = max($ids);
 
-    //prüfen ob die ID die höchste ist
-    $ids = [];
-    foreach ($xml->fahrten as $fahrt) {
-        $ids[] = (string) $fahrt['id'];
-    }
-    $maxId = max($ids);
-
-    // Suche die Fahrt mit der entsprechenden ID
-    foreach ($xml->fahrten as $fahrt) {
-        if ((string) $fahrt['id'] === $id) {
-            $id = (string)$fahrt['id'];
-            $name = $fahrt->name;
-            $wohnort = $fahrt->wohnort;
-            $zweck = $fahrt->zweck;
-            $datum = $fahrt->datum;
-            $uhrzeit_von = isset($fahrt->uhrzeit_von) ? $fahrt->uhrzeit_von : 'Nicht eingegeben';
-            $uhrzeit_bis = isset($fahrt->uhrzeit_bis) ? $fahrt->uhrzeit_bis : 'Nicht eingegeben';
-            $km_start = $fahrt->km_start ? $fahrt->km_start : 'Nicht eingegeben';;
-            $km_end = isset($fahrt->km_end) ? $fahrt->km_end : 'Nicht eingegeben'; // Default-Wert, falls kein Wert vorhanden
-            $kmdiff = isset($fahrt->kmdiff) ? $fahrt->kmdiff : ''; // Default-Wert, falls kein Wert vorhanden
+        // Suche die Fahrt mit der entsprechenden ID
+        foreach ($xml->fahrten as $fahrt) {
+            if ((string) $fahrt['id'] === $id) {
+                $id = (string) $fahrt['id'];
+                $name = $fahrt->name;
+                $wohnort = $fahrt->wohnort;
+                $zweck = $fahrt->zweck;
+                $datum = $fahrt->datum;
+                $uhrzeit_von = isset($fahrt->uhrzeit_von) ? $fahrt->uhrzeit_von : 'Nicht eingegeben';
+                $uhrzeit_bis = isset($fahrt->uhrzeit_bis) ? $fahrt->uhrzeit_bis : 'Nicht eingegeben';
+                $km_start = $fahrt->km_start ? $fahrt->km_start : 'Nicht eingegeben';
+                ;
+                $km_end = isset($fahrt->km_end) ? $fahrt->km_end : 'Nicht eingegeben'; // Default-Wert, falls kein Wert vorhanden
+                $kmdiff = isset($fahrt->kmdiff) ? $fahrt->kmdiff : ''; // Default-Wert, falls kein Wert vorhanden
+            }
         }
     }
-}
-?>
-    <h1>Details für Fahrt-ID:<?php echo htmlspecialchars($id); ?></h1>
-    <p>Name: <?php echo htmlspecialchars($name); ?></p>
-    <p>
-        Wohnort: <?php echo htmlspecialchars($wohnort); ?>
-    </p>
-    <p>Zweck: <?php echo htmlspecialchars($zweck); ?></p>
-    <p>Datum: <?php echo htmlspecialchars($datum); ?></p>
-    <p>Uhrzeit von: <?php echo htmlspecialchars($uhrzeit_von); ?></p>
-    <p>Uhrzeit bis: <?php echo htmlspecialchars($uhrzeit_bis); ?></p>
-    <p>Km Ende: <?php echo htmlspecialchars($km_end); ?></p>
-    <p>Km Diff: <?php echo htmlspecialchars($kmdiff); ?></p>
+    ?>
+    <div class="container">
+        <fieldset>
+            <h1 style=" color:black; padding:20px; text-align:center;">Details </h1>
+            <table>
+                <tr style="margin-top:40px;">
+                    <th>Name:</th>
+                    <td><?php echo htmlspecialchars($name); ?></td>
+                </tr>
+                <tr>
+                    <th>Wohnort:</th>
+                    <td><?php echo htmlspecialchars($wohnort); ?></td>
+                </tr>
+                <tr>
+                    <th>Zweck:</th>
+                    <td><?php echo htmlspecialchars($zweck); ?></td>
+                </tr>
+                <tr>
+                    <th>Datum:</th>
+                    <td><?php echo htmlspecialchars($datum); ?></td>
+                </tr>
+                <tr>
+                    <th>Uhrzeit von:</th>
+                    <td><?php echo htmlspecialchars($uhrzeit_von); ?></td>
+                </tr>
+                <tr>
+                    <th>Uhrzeit bis:</th>
+                    <td><?php echo htmlspecialchars($uhrzeit_bis); ?></td>
+                </tr>
+                <tr>
+                    <th>Km Start:</th>
+                    <td><?php echo htmlspecialchars($km_start); ?></td>
+                </tr>
+                <tr>
+                    <th>Km Ende:</th>
+                    <td><?php echo htmlspecialchars($km_end); ?></td>
+                </tr>
+                <tr>
+                    <th>Km Diff:</th>
+                    <td><?php echo htmlspecialchars($kmdiff); ?></td>
+                </tr>
+            </table>
+            <?php if ($id == $maxId): ?>
+                <form action="editDetailFile.php" method="post">
+                    <input type="hidden" name="form_type" value="form2">
+                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($fahrt['id']); ?>">
+                    <input type="hidden" name="datum" value="<?php echo htmlspecialchars($datum); ?>">
+                    <input type="hidden" name="uhrzeit_von" value="<?php echo htmlspecialchars($uhrzeit_von); ?>">
+                    <input type="hidden" name="uhrzeit_bis" value="<?php echo htmlspecialchars($uhrzeit_bis); ?>">
+                    <input type="hidden" name="name" value="<?php echo htmlspecialchars($name); ?>">
+                    <input type="hidden" name="wohnort" value="<?php echo htmlspecialchars($wohnort); ?>">
+                    <input type="hidden" name="zweck" value="<?php echo htmlspecialchars($zweck); ?>">
+                    <input type="hidden" name="km_start" value="<?php echo htmlspecialchars($km_start); ?>">
+                    <input type="hidden" name="km_end" value="<?php echo htmlspecialchars($km_end); ?>">
+                    <button type="submit" style="display:block; margin:auto; margin-left:120px;">Bearbeiten <i
+                            class="fa-solid fa-pen"></i></button>
+                </form>
+                <form action="ausgabe.php" method="get">
 
-    <?php if ($id == $maxId): ?>
-        <form action="editDetailFile.php" method="post">
-            <input type="hidden" name="form_type" value="form2">
-            <input type="hidden" name="id" value="<?php echo htmlspecialchars($fahrt['id']); ?>">
-            <input type="hidden" name="datum" value="<?php echo htmlspecialchars($datum); ?>">
-            <input type="hidden" name="uhrzeit_von" value="<?php echo htmlspecialchars($uhrzeit_von); ?>">
-            <input type="hidden" name="uhrzeit_bis" value="<?php echo htmlspecialchars($uhrzeit_bis); ?>">
-            <input type="hidden" name="name" value="<?php echo htmlspecialchars($name); ?>">
-            <input type="hidden" name="wohnort" value="<?php echo htmlspecialchars($wohnort); ?>">
-            <input type="hidden" name="zweck" value="<?php echo htmlspecialchars($zweck); ?>">
-            <input type="hidden" name="km_start" value="<?php echo htmlspecialchars($km_start); ?>">
-            <input type="hidden" name="km_end" value="<?php echo htmlspecialchars($km_end); ?>">
+                    <button style="display:block; margin:auto; margin-left:120px;">Schließen <i
+                            class="fa-regular fa-circle-xmark"></i></button>
+                </form>
 
-            <button type="submit" style="display:block; margin:auto;">Bearbeiten</button>
-        </form>
-    <?php else: ?>
-        <button style="display:block; margin:auto;" disabled>Bearbeiten</button>
-    <?php endif; ?>
+            <?php else: ?>
+                <form action="ausgabe.php" method="get">
+
+                    <button style="display:block; margin:auto;">Schließen <i
+                            class="fa-regular fa-circle-xmark"></i></button>
+                </form>
+            <?php endif; ?>
 
 
+        </fieldset>
 
-
-
+    </div>
 </body>
 
 </html>
